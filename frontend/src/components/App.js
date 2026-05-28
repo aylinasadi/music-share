642d997c32eeaaa36a356753891f2293ae14db52
aylinasadi <aylinasadi97@gmail.com>
2026-05-28 18:41:24 +0400

    fixed typo

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
     }
 
     render() {
-        return (<div classname="center-container">
+        return (<div className="center-container">
             <HomePage />
             </div>
         );
