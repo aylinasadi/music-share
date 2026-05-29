aa9bf8e53d9af8b2e02df07553705b8b7179a3b1
aylinasadi <aylinasadi97@gmail.com>
2026-05-29 15:33:20 +0400

    added .env

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,6 @@
-CLIENT_ID = "419dff5b9b9b4c88be224d62ade5c3ad"
-CLIENT_SECRET = "72fb84b52b994ff0b101d4868033e7dd"
-REDIRECT_URI = "https://music-share.up.railway.app/spotify/redirect"
+import os
+
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+REDIRECT_URI = os.environ.get('REDIRECT_URI', 'http://127.0.0.1:8000/spotify/redirect')
